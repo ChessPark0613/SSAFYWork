46981f4ed23971294d65ecc05bdcf279a09f30f7
Ha JuHyung <gk5524388@naver.com>
2025-09-03 09:15:39 +0900

    Init: new submodule Advanced init

---
+++ algorithm/ws_16/ws_16_3.py
@@ -0,0 +1,31 @@
+import sys
+
+
+def solve(n):
+    cnt = 0
+    ans = ""
+
+    while n > 0 and cnt < 13:
+        n = n * 2
+        if n >= 1:
+            ans += "1"
+            n -= 1
+        else:
+            ans += "0"
+
+        cnt += 1
+
+    if cnt == 13:
+        ans = "overflow"
+
+    print(f"#{tc} {ans}")
+
+
+if __name__ == "__main__":
+    sys.stdin = open("./ws_16_3_in.txt", "r")
+
+    T = int(input())
+
+    for tc in range(1, T + 1):
+        N = float(input().strip())
+        solve(N)
